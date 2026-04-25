51c50430ff1fc4b9cb5676ff3e61d6265cb3d0b5
Cristopher Vilchez <cristopher_vilchez@usmp.pe>
2026-04-24 22:03:10 -0500

    Proyecto base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Parcial_Vilchez_Cristopher.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
